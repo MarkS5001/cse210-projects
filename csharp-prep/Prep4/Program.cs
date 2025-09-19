95b69d7b4ef0f3f6a835f53835e983fada313571
Mark Smith <smi20046@byui.edu>
2025-09-19 10:34:24 -0600

    Made list sum, average, max program.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int addedNumber = 1;
+        while (addedNumber != 0)
+        {
+            Console.Write("Enter Number: ");
+            addedNumber = int.Parse(Console.ReadLine());
+            if (addedNumber != 0)
+            {
+                numbers.Add(addedNumber);
+            }
+            
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        Console.WriteLine($"The average is: {sum / numbers.Count}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
     }
 }
