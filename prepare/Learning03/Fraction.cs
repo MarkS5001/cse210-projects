3acca5d5b75ee4903cb9a45a0080a16be2966401
Mark Smith <smi20046@byui.edu>
2025-10-11 10:59:54 -0600

    Made fraction program

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,56 @@
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetTop()
+    {
+        Console.WriteLine(_top);
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void GetBottom()
+    {
+        Console.WriteLine(_bottom);
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        // Either way works
+        // double frac = (double)_top / _bottom;
+        // return frac;
+        return (double)_top / _bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("bob");
+        Fraction bob = new Fraction();
+        bob.GetTop();
+        bob.GetBottom();
+        bob.SetTop(6);
+        Console.WriteLine(bob.GetFractionString());
+        Console.WriteLine(bob.GetDecimalValue());
+
+        Console.WriteLine("jim");
+        Fraction jim = new Fraction(6);
+        jim.GetTop();
+        jim.GetBottom();
+        jim.SetBottom(2);
+        Console.WriteLine(jim.GetFractionString());
+        Console.WriteLine(jim.GetDecimalValue());
+
+        Console.WriteLine("ty");
+        Fraction ty = new Fraction(6, 7);
+        ty.GetTop();
+        ty.GetBottom();
+        Console.WriteLine(ty.GetFractionString());
+        Console.WriteLine(ty.GetDecimalValue());
     }
 }
