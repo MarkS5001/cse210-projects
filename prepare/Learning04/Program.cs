3628ee37f0b933e9f9b90d88e31ca58d30a5f878
Mark Smith <smi20046@byui.edu>
2025-10-24 20:33:01 -0600

    Followed the prepare activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment bill = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(bill.GetSummary());
+
+        MathAssignment bob = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(bob.GetSummary());
+        Console.WriteLine(bob.GetHomeworkList());
+
+        WritingAssignment joel = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(joel.GetSummary());
+        Console.WriteLine(joel.GetWritingInformation());
     }
 }
