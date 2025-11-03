37f500b6c04fc6a1719096a1091a97f2afa27c48
Mark Smith <smi20046@byui.edu>
2025-11-03 11:21:15 -0700

    Added comments

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,6 +2,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Initialize all needed variables
         Breathing breathe = new Breathing("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
         Reflection reflection = new Reflection("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
         Listing listing = new Listing("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
