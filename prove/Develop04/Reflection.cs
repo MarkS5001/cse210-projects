4fb50695deed6676d621313ea4402f088cf3536e
Mark Smith <smi20046@byui.edu>
2025-11-03 10:51:22 -0700

    Minor fixes

--- prove/Develop04/Reflection.cs
+++ prove/Develop04/Reflection.cs
@@ -4,26 +4,25 @@ class Reflection : Activity
 {
     private List<string> _prompts = new List<string> { "1", "2", "3" }; // List for prompts
     private List<string> _questions = new List<string> { "1", "2", "3" }; // List for questions
-    private Random random = new Random(); // Random seed for random prompt, and question
+    private Random _random = new Random(); // Random seed for random prompt, and question
     private int _index; // Stores random index for prompt, and question
     private string _currentPrompt; // Stores current prompt
     private string _currentQuestion; // Stores current question
     private DateTime _startTime; // Stores start time for loop
     private DateTime _futureTime; // Stores end time for loop
-    private int _count; // Stores count of questions answered
     public Reflection(string name, string description) : base(name, description)
     {
 
     }
     public void GetPrompt() // Get random prompt
     {
-        _index = random.Next(0, _prompts.Count);
+        _index = _random.Next(0, _prompts.Count);
         _currentPrompt = _prompts[_index];
         _prompts.RemoveAt(_index);
     }
     public void GetQuestion() // Get random question
     {
-        _index = random.Next(0, _questions.Count);
+        _index = _random.Next(0, _questions.Count);
         _currentQuestion = _questions[_index];
         _questions.RemoveAt(_index);
     }
@@ -45,16 +44,14 @@ class Reflection : Activity
         Console.Clear();
 
         // Initialize variables for loop
-        _count = 0;
         _startTime = DateTime.Now;
         _futureTime = _startTime.AddSeconds(_time);
 
-        while (_startTime < _futureTime)
+        while (DateTime.Now < _futureTime)
         {
             GetQuestion();
             Console.WriteLine($"> {_currentQuestion}");
             Spinner();
-            _count++;
             Console.WriteLine();
         }
 
