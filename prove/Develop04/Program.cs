d887d7fdf9d5968496e5cc9ca1ff2d96e3297bbf
Mark Smith <smi20046@byui.edu>
2025-11-03 11:09:17 -0700

    Wrote program logic

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,39 +1,35 @@
-using System;
-
 class Program
 {
-    int _userChoice; // Keep track of user choice
-    public void Menu() // Provide menu
-    {
-        Console.Clear();
-        Console.WriteLine("Menu Options:");
-        Console.WriteLine("  1. Start breathing activity");
-        Console.WriteLine("  2. Start reflecting activity");
-        Console.WriteLine("  3. Start listing activity");
-        Console.WriteLine("  4. Quit");
-        Console.WriteLine("Select a choice from the menu: ");
-        _userChoice = int.Parse(Console.ReadLine());
-    }
-    public void choice() // Do choice
-    {
-        if (_userChoice == 1)
-        {
-            breath
-        }
-        else if (_userChoice == 2) {
-            reflection
-        }
-        else if (_userChoice == 3)
-        {
-            lisiting
-        }
-        else if (_userChoice == 4)
-        {
-            
-        }
-    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Breathing breathe = new Breathing("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        Reflection reflection = new Reflection("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+        Listing listing = new Listing("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        int _userChoice; // Keep track of user choice
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.WriteLine("Select a choice from the menu: ");
+            _userChoice = int.Parse(Console.ReadLine());
+
+            if (_userChoice == 1)
+            {
+                breathe.BreatheLoop();
+            }
+            else if (_userChoice == 2)
+            {
+                reflection.StartReflection();
+            }
+            else if (_userChoice == 3)
+            {
+                listing.StartListing();
+            }
+        } while (_userChoice != 4);
     }
 }
