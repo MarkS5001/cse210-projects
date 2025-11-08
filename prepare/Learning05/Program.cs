e164ac61291a29acc2e2ab30bca2b581b69211bf
Mark Smith <smi20046@byui.edu>
2025-11-08 10:20:05 -0700

    Finished the prep

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.Clear();
+
+        Square square = new Square("orange", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("red", 5, 10);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("yellow", 5);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        // List<Shape> shapes = new List<Shape>();
+        // shapes.Add(square);
+        // shapes.Add(rectangle);
+        // shapes.Add(circle);
+        List<Shape> shapes = new List<Shape> {square, rectangle, circle};
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
